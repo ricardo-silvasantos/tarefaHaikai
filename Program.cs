14ecb6e759c4cedeadada09d0029d9a5cb62cd97
ricardo-silvasantos <ricosilvasantos@yahoo.com.br>
2023-08-19 22:45:45 -0300

    resolução do exercicio Haikai

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("OS ANDAIMES");
+Console.WriteLine("");
+Console.WriteLine("Na gaiola cheia");
+Console.WriteLine("(pedreiros e carpinteiros)");   
+Console.WriteLine("o dia gorjeia.");
+Console.WriteLine("");
+Console.WriteLine("Guilherme de Almeida");
